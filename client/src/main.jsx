5f500a8fa09d928a84f7897d0c8a5337f832a36e
Roshan <roshanbhagatwork@gmail.com>
2025-04-20 22:32:09 +0530

    removed strict mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
@@ -9,9 +8,7 @@ import { AuthContextProvider } from "./Contexts/AuthContext.jsx";
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
     <AuthContextProvider>
       <RouterProvider router={router} />
     </AuthContextProvider>
-  </StrictMode>
 );
